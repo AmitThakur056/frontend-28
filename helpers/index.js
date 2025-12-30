909d01cae06612b70b204b0ba187a5827524b359
AmitThakur056 <amitthakur.techgeek@gmail.com>
2025-12-30 14:50:03 +0530

    Fix formatting in error handler function

--- helpers/index.js
+++ helpers/index.js
@@ -10,7 +10,7 @@
    * var app = express();
    * app.use(helpers.errorHandler);
    * */
-
+    
   helpers.errorHandler = function(err, req, res, next) {
     var ret = {
       message: err.message,
